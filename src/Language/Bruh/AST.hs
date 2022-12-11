19057ccd602c6aee12fe789558efbfbfef2762bd
Yulia Gorshkova <yuuri.sumiren@gmail.com>
2022-12-12 02:16:21 +0300

    Arithmetic expressions

--- src/Language/Bruh/AST.hs
+++ src/Language/Bruh/AST.hs
@@ -1,6 +1,33 @@
-module Language.Bruh.AST
-  ( someFunc
-  ) where
+module Language.Bruh.AST where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.Text (Text)
+
+{-| Variables. |-}
+newtype Variable = VVar { vName :: Text }
+  deriving (Eq, Show)
+
+data UnOp = UNeg
+  deriving (Eq, Show)
+
+data BinOp
+  = BAdd
+  | BSub
+  | BMul
+  deriving (Eq, Show)
+
+{-| Expressions. |-}
+data Expr
+  = EInteger Int             -- ^ Integral literal
+  | EVar     Variable        -- ^ Variable
+  | EUnary   UnOp  Expr      -- ^ Unary operator
+  | EBinary  BinOp Expr Expr -- ^ Binary operator
+  deriving (Eq, Show)
+
+instance Num Expr where
+  fromInteger = EInteger . fromInteger
+  (+)    = EBinary BAdd
+  (-)    = EBinary BSub
+  (*)    = EBinary BMul
+  negate = EUnary UNeg
+  abs    = error "abs is not implemented for Expr"
+  signum = error "signum is not implemented for Expr"
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,8 @@
+import Test.Hspec
+
+import Language.Bruh.AST
+    
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $ do
+  it "can parse expressions" $ do
+    2 + 2 `shouldBe` EBinary BAdd (EInteger 2) (EInteger 2)
